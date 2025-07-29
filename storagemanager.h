3f10ef4bf416a3a5edc8ed2a0d38c64a3597f646
igr47 <njagielvis44@gmail.com>
2025-07-30 01:41:04 +0300

    Fixed a segmentation fault error.

--- storagemanager.h
+++ storagemanager.h
@@ -10,15 +10,36 @@
 #include <fstream>
 #include <mutex>
 #include <array>
+#include <stdexcept>
 
-inline constexpr uint32_t DB_PAGE_SIZE = 1024;
+inline constexpr uint32_t DB_PAGE_SIZE = 4096;
+inline constexpr size_t DEFAULT_BUFFER_POOL_CAPACITY=1000;
+inline constexpr uint32_t DEFAULT_BPTREE_ORDER=100;
+
+class StorageException : public std::runtime_error {
+public:
+    using std::runtime_error::runtime_error;
+};
+
+class FileIOException:public StorageException{
+	public:
+		using StorageException::StorageException;
+};
+
+class IntegrityViolation:public StorageException{
+	public:
+		using StorageException::StorageException;
+};
 
 class Page {
 public:
     uint32_t page_id;
     bool dirty;
     std::array<uint8_t, DB_PAGE_SIZE> data;
-    Page(uint32_t id);
+    
+    explicit Page(uint32_t id) noexcept;
+    Page(const Page&) = delete;
+    Page& operator=(const Page&) = delete;
 };
 
 class Pager {
@@ -28,33 +49,51 @@ private:
     std::unordered_map<uint32_t, std::shared_ptr<Page>> page_cache;
     std::mutex cache_mutex;
     uint32_t next_page_id = 0;
+
+    void ensure_file_open();
+    bool safe_seek(uint32_t page_id) noexcept;
+    
 public:
     explicit Pager(const std::string& db_file);
-    ~Pager();
+    ~Pager() noexcept;
+
+    // Disallow copying
+    Pager(const Pager&) = delete;
+    Pager& operator=(const Pager&) = delete;
 
     std::shared_ptr<Page> get_page(uint32_t page_id);
     uint32_t allocate_page();
     void mark_dirty(uint32_t page_id);
-    void flush_all();
+    void flush_all() noexcept;
+    
 public:
-    bool read_page_from_disk(uint32_t page_id, std::array<uint8_t, DB_PAGE_SIZE>& buffer);
-    bool write_page_to_disk(uint32_t page_id, const std::array<uint8_t, DB_PAGE_SIZE>& buffer);
+    bool read_page_from_disk(uint32_t page_id, std::array<uint8_t, DB_PAGE_SIZE>& buffer) noexcept;
+    bool write_page_to_disk(uint32_t page_id, const std::array<uint8_t, DB_PAGE_SIZE>& buffer) noexcept;
     void load_metadata();
-    void save_metadata();
+    void save_metadata() noexcept;
+    uint32_t get_next_page_id() const;
 };
 
 class BufferPool {
 private:
     Pager& pager;
-    size_t capacity;
+    const size_t capacity;
     std::unordered_map<uint32_t, std::shared_ptr<Page>> pool;
     std::list<uint32_t> lru_list;
     std::unordered_map<uint32_t, std::list<uint32_t>::iterator> lru_map;
     std::mutex mutex;
+
+    void evict_page() noexcept;
+    
 public:
-    BufferPool(Pager& pager, size_t capacity = 1000);
+    BufferPool(Pager& pager, size_t capacity = 1000) noexcept;
+    
+    // Disallow copying
+    BufferPool(const BufferPool&) = delete;
+    BufferPool& operator=(const BufferPool&) = delete;
+
     std::shared_ptr<Page> fetch_page(uint32_t page_id);
-    void flush_all();
+    void flush_all() noexcept;
 };
 
 class WriteAheadLog {
@@ -62,12 +101,21 @@ private:
     std::string log_file_path;
     std::fstream log_file;
     std::mutex mutex;
+
+    void ensure_file_open();
+    
 public:
     explicit WriteAheadLog(const std::string& path);
+    ~WriteAheadLog() noexcept;
+
+    // Disallow copying
+    WriteAheadLog(const WriteAheadLog&) = delete;
+    WriteAheadLog& operator=(const WriteAheadLog&) = delete;
+
     void log_transaction_begin(uint64_t tx_id);
-    void log_page_write(uint64_t tx_id, uint32_t page_id, 
-                       const std::array<uint8_t, DB_PAGE_SIZE>& old_data,
-                       const std::array<uint8_t, DB_PAGE_SIZE>& new_data);
+    void log_page_write(uint64_t tx_id, uint32_t page_id,
+                      const std::array<uint8_t, DB_PAGE_SIZE>& old_data,
+                      const std::array<uint8_t, DB_PAGE_SIZE>& new_data);
     void log_transaction_commit(uint64_t tx_id);
     void log_check_point();
     void recover();
@@ -78,7 +126,8 @@ private:
     Pager& pager;
     BufferPool& buffer_pool;
     uint32_t root_page_id;
-    uint32_t order;
+    const uint32_t order;
+
     struct Node {
         bool is_leaf;
         uint32_t num_keys;
@@ -86,15 +135,24 @@ private:
         std::vector<uint32_t> children;
         std::vector<std::vector<uint8_t>> values;
     };
+
+    void validate_node(const Node& node) const;
+    
 public:
     BPlusTree(Pager& pager, BufferPool& buffer_pool, uint32_t order = 100);
+    
+    // Disallow copying
+    BPlusTree(const BPlusTree&) = delete;
+    BPlusTree& operator=(const BPlusTree&) = delete;
+
     void insert(uint32_t key, const std::vector<uint8_t>& value);
     std::vector<uint8_t> search(uint32_t key);
+    
 private:
     void insert_non_full(Node& node, uint32_t key, const std::vector<uint8_t>& value);
     void split_child(Node& parent, uint32_t index, Node& child);
-    void serialize_node(const Node& node, std::array<uint8_t, DB_PAGE_SIZE>& buffer);
-    void deserialize_node(const std::array<uint8_t, DB_PAGE_SIZE>& buffer, Node& node);
+    void serialize_node(const Node& node, std::array<uint8_t, DB_PAGE_SIZE>& buffer) const;
+    void deserialize_node(const std::array<uint8_t, DB_PAGE_SIZE>& buffer, Node& node) const;
 };
 
-#endif
+#endif // STORAGE_MANAGER_H
